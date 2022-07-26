76a32adee5b2e2b1bbc6ae7bc808cc72a9d8ea40
badjok3 <dani.popoff@abv.bg>
2022-07-26 15:18:54 +0300

    Added project base.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Duende.IdentityServer.EntityFramework.Options;
+using stock_attendant.Models;
+
+namespace stock_attendant.Data;
+
+public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+{
+    public ApplicationDbContext(DbContextOptions options, IOptions<OperationalStoreOptions> operationalStoreOptions)
+        : base(options, operationalStoreOptions)
+    {
+
+    }
+}
